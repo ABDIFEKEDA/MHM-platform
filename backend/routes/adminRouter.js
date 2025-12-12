8b3d674b5bb66f3b80d9adab762b74672b11a739
ABDIFEKEDA <abdifk23@gmail.com.com>
2025-12-12 22:28:36 +0300

    add doctor dashboard and fix the other issues

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Admin-only patient management
 router.post("/", protect(["admin"]), addPatient);
-router.get("/patientslist", protect(["admin"]), listPatients);
+router.get("/", protect(["admin"]), listPatients);
 router.put("/:id", protect(["admin"]), editPatient);
 router.delete("/:id", protect(["admin"]), removePatient);
 
